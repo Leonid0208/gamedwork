29f213c4c40b6b57acc2fd26d46229b91598a8e4
LeonID0208 <leonid@DESKTOP-37F469Q.localdomain>
2021-10-23 13:47:59 +0300

    Add models

--- django_react/settings.py
+++ django_react/settings.py
@@ -81,11 +81,19 @@ WSGI_APPLICATION = 'django_react.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'gamedwork',
     }
 }
 
--- profile_user/models.py
+++ profile_user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Student(models.Model):
+    full_name = models.CharField(max_length=150)
+    birthDate = models.DateField(default=timezone.now)
+    email = models.EmailField(default='')
+    login = models.CharField(max_length=150, default='')
+    password = models.CharField(max_length=50, default='')
+    phone = models.CharField(max_length=100, default='')
+
+
+class Teacher(models.Model):
+    full_name = models.CharField(max_length=150)
+    birthDate = models.DateField(default=timezone.now)
+    email = models.EmailField(default='')
+    login = models.CharField(max_length=150, default='')
+    password = models.CharField(max_length=50, default='')
+    phone = models.CharField(max_length=100, default='')
